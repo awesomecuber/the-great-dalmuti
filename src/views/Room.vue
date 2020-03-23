50387678bb454b6eb9b7326a846e01c92ab28bae
Nicolas Brassard <awesomecuber10401@gmail.com>
2020-03-23 00:49:02 -0700

    beginning to implement ready

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -2,7 +2,21 @@
   <div class="room">
     <button @click="leave">Back to home</button>
     <h1>Room: {{ $route.params.room }}</h1>
-    <p v-for="(user, index) in users" :key="index">{{ user === name ? user + ' (you!)' : user }}</p>
+    <p>Once everyone is ready, the game will start</p>
+    <h3>Users:</h3>
+    <div v-for="(user, index) in users" :key="index">
+      <p v-if="user === name">
+        <b>{{ user }} </b>
+        <button @click="toggleReady"> ready: {{ ready ? 'yes' : 'no' }} </button>
+      </p>
+      <p v-else>
+        {{ user }}
+      </p>
+    </div>
+    <p v-if="users.length === 0">
+      <i>(no one)</i>
+    </p>
+    <br>
     <form v-if="!loggedIn" @submit.prevent="nick">
       <input type="text" v-model="nickInput">
       <input type="submit" value="login">
@@ -19,7 +33,8 @@ export default {
       users: [],
       nickInput: '',
       name: '',
-      loggedIn: false
+      loggedIn: false,
+      ready: false
     }
   },
   mounted() {
@@ -37,18 +52,21 @@ export default {
     })
 
     window.onbeforeunload = () => {
-      this.$socket.emit('user-left', this.$route.params.room)
+      this.$socket.emit('user-left', this.$route.params.room, this.name)
     }
 
     this.$socket.emit('request-rooms')
   },
   methods: {
     nick() {
-      if (this.nickInput !== '') {
+      if (this.nickInput !== '' && !this.users.includes(this.nickInput)) {
         this.name = this.nickInput
         this.loggedIn = true
         this.$socket.emit('user-joined', this.$route.params.room, this.name)
       }
+    },
+    toggleReady() {
+      this.ready = !this.ready
     },
     leave() {
       if (this.loggedIn) {
