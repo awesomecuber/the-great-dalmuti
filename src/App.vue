cf63361a0f96373901ff388bb540181d4875b379
nbrass <nbrass@uw.edu>
2020-11-03 04:25:17 -0800

    switch to vue-socket-io and remove server

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,25 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  sockets: {
+    roomListUpdate: function(roomList) {
+      this.$store.dispatch('updateRoomList', roomList)
+    },
+    userListUpdate: function(userList) {
+      this.$store.dispatch('updateUserList', userList)
+    },
+    gameStateUpdate: function(gameState) {
+      this.$store.dispatch('updateGameState', gameState)
+    },
+    userStateUpdate: function(userState) {
+      this.$store.dispatch('updateUserState', userState)
+    },
+    disconnect: function(reason) {
+      if (reason === 'io client disconnect') {
+        this.$socket.emit('user-left', this.$route.params.room)
+      }
+    }
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueSocketIO from 'vue-socket.io'
 import io from 'socket.io-client'
 import store from './store'
 
@@ -12,20 +13,11 @@ let socket = io(
     : 'http://localhost:3000'
 )
 
-socket.on('room-list-update', roomList => store.dispatch('updateRoomList', roomList))
-socket.on('user-list-update', userList => store.dispatch('updateUserList', userList))
-socket.on('game-state-update', gameState => store.dispatch('updateGameState', gameState))
-socket.on('user-state-update', userState => store.dispatch('updateUserState', userState))
-socket.on('disconnect', reason => {
-  if (reason === 'io client disconnect') {
-    this.$socket.emit('user-left', this.$route.params.room)
-  }
-})
-socket.on('connect', () => {
-  // store.dispatch('attemptReconnect', { id: socket.id })
-})
-
-Vue.prototype.$socket = socket
+Vue.use(
+  new VueSocketIO({
+    connection: socket
+  })
+)
 
 new Vue({
   router,
